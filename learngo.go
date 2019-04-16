f5ba4be6300f064bdaa1180d4270675f280e3e9f
Michael Krueger <mbh.krueger@gmail.com>
2019-04-16 15:27:43 +0200

    cli args

--- learngo.go
+++ learngo.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/mbhk/learngo/fibo"
 )
 
 func main() {
+	var iterations uint
+	flag.UintVar(&iterations, "iterations", 10, "number of iterations")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [-iterations n]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
 	f := fibo.Fibonacci{}
-	i := 0
-	for i < 20 {
+	i := uint(0)
+	for i < iterations {
 		i++
 		fmt.Println(f.Next())
 	}
